936a1ed03043d78fac11ee1c7830f33829f7e85e
daiki062417-blip <daiki062417@gmail.com>
2026-02-01 09:39:04 +0900

    Update aaa2.py

--- aaa2.py
+++ aaa2.py
@@ -1,1 +1,24 @@
+import streamlit as st
 
+data = []   #評価データを保存する
+
+contentValue = st.slider(
+    'タスクの内容に関する評価', 0, 10, key='content'
+)
+personValue = st.slider(
+    '引継ぎ相手に関する評価', 0, 10, key='person'
+)
+scheduleValue = st.slider(
+    'スケジュールに関する評価', 0, 10, key='schedule'
+)
+
+st.write("content:", contentValue)
+st.write("person:", personValue)
+st.write("schedule:", scheduleValue)
+
+data.append([contentValue, personValue, scheduleValue]) #評価データ作成
+
+
+with open("sample.csv", mode="w", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerows(data)
